03341bd3ac27371e9e81cb414ad9ff7a3ff18bee
Ashwin Narayanan <ashwin.narayanan@shopify.com>
2020-06-09 23:26:14 -0400

    ApexCharts components

---
+++ src/components/PopChart.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import Chart from "react-apexcharts";
+
+class PopChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: [44, 71, 67],
+      options: {
+        chart: {
+          height: 350,
+          type: "radialBar",
+        },
+        fill: {
+          colors: ["#6646ED", "#5ED6C3", "#2C50ED"],
+        },
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              name: {
+                fontSize: "22px",
+                color: "#FFFFFF",
+              },
+              value: {
+                fontSize: "16px",
+                color: "#FFFFFF",
+              },
+              total: {
+                show: true,
+                label: "Total",
+                color: "#FFFFFF",
+                formatter: function (w) {
+                  // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                  return 250;
+                },
+              },
+            },
+          },
+        },
+        labels: ["Brazil", "Russia", "India"],
+      },
+    };
+  }
+  render() {
+    return (
+      <Chart
+        options={this.state.options}
+        series={this.state.series}
+        type="radialBar"
+        height={350}
+      />
+    );
+  }
+}
+
+export default PopChart;
